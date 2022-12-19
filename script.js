6b42035f085aa854e2c703369d9ed764383f88e2
NazimS <nazim.shixkerimov@gmail.com>
2022-12-19 09:09:49 +0400

    variable changed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 const typing = document.getElementById("typing");
 let i = 0;
-let text = 'Baku is the most breath-taking city of the world.';
+const text = 'Baku is the most breath-taking city of the world.';
 
 setInterval(() => {
    if (i < text.length) {
